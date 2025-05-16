ff240b69a75aca6af3f045c0dc529a26f6de3ca6
kiko solovic <kisolovic@gmail.com>
2025-05-16 14:05:28 +0200

    added button for viewing result data

--- Views/About.axaml.cs
+++ Views/About.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Diagnostics;
+
+using Avalonia.Interactivity;
 
 namespace Heating_Optimization;
 
@@ -9,5 +12,14 @@ public partial class About : UserControl
     public About()
     {
         InitializeComponent();
+    }
+
+    public void showData(object sender, RoutedEventArgs args)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "ProductionCost_Selected.csv",
+            UseShellExecute = true
+        });
     }
 }
